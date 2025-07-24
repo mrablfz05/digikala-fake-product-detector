190f91d856aed2da3c318dacccd1a91080cddb63
mrablfz <abolfaz88888888@gmail.com>
2025-07-24 01:31:26 -0700

    chore: delete some files for solving problem

--- modules/imports.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
-
-from catboost import CatBoostClassifier
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import classification_report, confusion_matrix
